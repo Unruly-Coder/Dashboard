55c78d90f3b01f24ced261e3c8ab61aa0841a3b0
Crazy Ivan <pawel.brod@gmail.com>
2014-03-04 22:15:15 +0100

    add particles

---
+++ client/app/shared/directives/particle.js
@@ -0,0 +1,53 @@
+angular.module('app').directive('particle', function() {
+    return {
+        restrict: 'E',
+
+        template: '<canvas></canvas>',
+        replace: true,
+        scope: { },
+        link: function (scope, elm, attrs, controller) {
+
+            var context = elm[0].getContext('2d'),
+                screenWidth = window.innerWidth,
+                screenHeight = window.innerHeight,
+                firstEmitter = new jEmitter.ParticleEmitter({
+                    spreadX : 0,
+                    spreadY : window.innerHeight/2,
+                    minVelocity : 3,
+                    maxVelocity : 3,
+                    minGravity : 0,
+                    maxGravity : 0,
+                    minWind	   : 0,
+                    maxWind	   : 10,
+                    minSize    : 5,
+                    maxSize    : 15,
+                    minSizeStep : -3,
+                    maxSizeStep : -3,
+                    maxParticleEmit : 1,
+                    minParticleEmit : 0,
+                    colors: ['89A4F0']
+                });
+
+            console.log(firstEmitter);
+
+            function animate() {
+                context.clearRect(0,0,screenWidth,screenHeight);
+                firstEmitter.render(context);
+                window.requestAnimationFrame(animate);
+            };
+
+            elm[0].width = screenWidth;
+            elm[0].height = screenHeight;
+            elm.css({
+                opacity: 0.3,
+                position: 'fixed'
+            });
+
+            animate();
+
+             setInterval(function() {
+             		firstEmitter.emit(-10,screenHeight/2);
+             }, 50);
+        }
+    }
+});
