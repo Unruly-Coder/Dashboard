ae5515f4182c6729ae1a158ada230af1f0211c59
Crazy-Ivan <pawel.brod@gmail.com>
2014-04-08 20:03:14 +0200

    update weather widget

--- client/app/widgets/weather/weatherCtrl.js
+++ client/app/widgets/weather/weatherCtrl.js
@@ -1,7 +1,20 @@
 angular.module('dashboard')
 
     .controller('weatherEditCtrl', ['$scope', '$collection', function($scope, $collection) {
-        var cityCollection = $collection.getInstance();
+
+
+        var cityCollection = $collection.getInstance(),
+            icon =$scope.options.data.weather[0].icon,
+            colors = {
+                good : '#e9367b',
+                bad : '#008b9c'
+            };
+
+        if(icon === "01d" || icon === "02d" ||  icon === "01n" ||  icon === "02n") {
+            $scope.options.color = colors.good;
+        } else {
+            $scope.options.color = colors.bad;
+        }
 
         cityCollection.addAll([
             {city: 'Kraków', 'url': 'http://api.openweathermap.org/data/2.5/weather?id=3094802&units=metric'},
--- modules/routes/userImage/index.js
+++ modules/routes/userImage/index.js
@@ -4,14 +4,15 @@ module.exports = function setup(options, imports, register) {
         servicehub = imports.servicehub;
 
     function userImageHandler(request, response) {
+
         servicehub.getData(options.url + request.params.id).then(function(data) {
+
             response.set('Content-Type', 'image/jpeg');
             response.send(data);
         });
     }
 
     webserver.app.get('/user/:id.jpg', userImageHandler);
-
 
     register(null, {});
 };
